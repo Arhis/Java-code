47f1316ed204d774ae09c35e608c6d4c3c9ed1d3
Arhis <avanture_boy@mail.ru>
2017-03-18 20:32:09 +0200

    Update CasioCalc.java

--- CasioCalc.java
+++ CasioCalc.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package casiocalc;
 
 import javafx.application.Application;
@@ -13,7 +8,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author Виктор
+ * @author Arhis
  */
 public class CasioCalc extends Application {
     
